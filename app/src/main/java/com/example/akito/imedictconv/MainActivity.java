df43ca3c7424a694ff5e0c7c9418930186be51bc
AkitoMizuno <garuku@outlook.jp>
2016-02-07 19:50:33 +0900

    Sample移植

--- app/src/main/java/com/example/akito/imedictconv/MainActivity.java
+++ app/src/main/java/com/example/akito/imedictconv/MainActivity.java
@@ -1,32 +1,236 @@
 package com.example.akito.imedictconv;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] IME_Name;
+    private TextView debugTv;
+    private Button inBtn;
+    private Button outBtn;
+    private int inPosition = -1;
+    private int outPosition = -1;
+    private com.example.akito.imedictconv.UserDictionaryConverter userDicCon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        debugTv = (TextView) findViewById(R.id.debugtv);
+        inBtn = (Button) findViewById(R.id.inbtn);
+        outBtn = (Button) findViewById(R.id.outbtn);
+        IME_Name = getResources().getStringArray(R.array.IMEList);
+        userDicCon = new com.example.akito.imedictconv.UserDictionaryConverter();
+
+        //ボタンで英小文字を表示できるように設定
+        inBtn.setAllCaps(false);
+        outBtn.setAllCaps(false);
+
+        //ボタンを押したら選択ダイアログ表示
+        inBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                showSelectionDialog(inPosition, true, "移行元のIMEを選択してください");
+            }
+        });
+        outBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSelectionDialog(outPosition, false, "移行先のIMEを選択してください");
+            }
+        });
+
+        findViewById(R.id.actionbt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPosition()) {
+                    receiveConvert();
+                }
             }
         });
     }
+
+
+    //チェックボタンダイアログの表示
+    private void showSelectionDialog(int position, final boolean isInbtn, String title) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setSingleChoiceItems(IME_Name, position, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        //選択されたIMEの位置を取得
+                        if (isInbtn) {
+                            inBtn.setText("移行元:" + IME_Name[item]);
+                            inPosition = item;
+                        } else {
+                            outBtn.setText("移行先:" + IME_Name[item]);
+                            outPosition = item;
+                        }
+                        debugTv.setText(String.valueOf(inPosition) + ", " + String.valueOf(outPosition));
+                        dialog.cancel();
+                    }
+                }).show();
+    }
+
+    //変換ボタンが押されたら最初に正しいラジオボタンが選択されているかチェック
+    private boolean checkPosition() {
+        AlertDialog.Builder aldb = new AlertDialog.Builder(this);
+        aldb.setTitle("警告");
+        aldb.setPositiveButton("OK", null);
+
+        if (inPosition == -1 && outPosition == -1) {
+            aldb.setMessage("IMEを選択してください").show();
+            return false;
+        } else if (inPosition == outPosition) {
+            aldb.setMessage("異なるIMEを選択してください").show();
+            return false;
+        } else if (inPosition == -1) {
+            aldb.setMessage("移行元のIMEを選択してください").show();
+            return false;
+        } else if (outPosition == -1) {
+            aldb.setMessage("移行先のIMEを選択してください").show();
+            return false;
+        }
+        return true;
+    }
+
+    //チェックを通ったらユーザーに確認して変換開始
+    private void receiveConvert() {
+        new AlertDialog.Builder(this)
+                .setMessage(IME_Name[inPosition] + "のユーザー辞書を" + IME_Name[outPosition] + "用に変換しますか？")
+                .setPositiveButton("変換", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startConverter();
+                    }
+                })
+                .setNegativeButton("キャンセル", null)
+                .show();
+    }
+
+
+
+    //移行元IMEの取得
+    private void startConverter() {
+        try {
+            switch (inPosition) {
+                case 0:
+                    selectConverterFromGoogle();
+                    break;
+                case 1:
+                    Toast.makeText(this, "すみません未実装です", Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    selectConverterFromSimeji();
+                    break;
+                case 3:
+                    selectConverterFromWnn();
+                    break;
+                case 4:
+                    selectConverterFromPobox();
+                    break;
+                case 5:
+                    Toast.makeText(this, "すみません未実装です", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    debugTv.setText(String.valueOf(inPosition) + ", " + String.valueOf(outPosition));
+            }
+            debugTv.setText(userDicCon.getOutputPath());
+        } catch (IOException | IndexOutOfBoundsException e) {
+            debugTv.setText(e.toString());
+            Toast.makeText(this, "例外が発生しました", Toast.LENGTH_SHORT).show();
+        } finally {
+            userDicCon.closeFile();
+        }
+    }
+
+
+    //移行元IME毎のメソッドを呼ぶ
+
+    private void selectConverterFromGoogle() throws IOException, IndexOutOfBoundsException {
+        userDicCon.setInputPath("GoogleIMEDic.txt");
+        userDicCon.setOutputPath(outPosition);
+        switch (outPosition) {
+            case 2:
+                userDicCon.convertToSimeji(this, "\t", "UTF-8");
+                break;
+            case 3:
+                userDicCon.convertNormal(this, "\t", " ", "UTF-8", "\n");
+                break;
+            case 4:
+                userDicCon.convertNormal(this, "\t", "\t", "UTF-8", "\r\n");
+                break;
+            default:
+                Toast.makeText(this, "すみません未実装です", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void selectConverterFromSimeji() throws IOException, IndexOutOfBoundsException {
+        userDicCon.setInputPath("Simeji/simeji_user_dic.txt");
+        userDicCon.setOutputPath(outPosition);
+        switch (outPosition) {
+            case 0:
+                userDicCon.convertFromSimeji(this, "\t", "UTF-8", "\t名\t\n");
+                break;
+            case 3:
+                userDicCon.convertFromSimeji(this, " ", "UTF-8", "\n");
+                break;
+            case 4:
+                userDicCon.convertFromSimeji(this, "\t", "UTF-8", "\r\n");
+                break;
+            default:
+                Toast.makeText(this, "すみません未実装です", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void selectConverterFromWnn() throws IOException, IndexOutOfBoundsException {
+        userDicCon.setInputPath("FlickWnn/user_dic.txt");
+        userDicCon.setOutputPath(outPosition);
+        switch (outPosition) {
+            case 0:
+                userDicCon.convertNormal(this, " ", "\t", "UTF-8", "\t名\t\n");
+                break;
+            case 2:
+                userDicCon.convertToSimeji(this, " ", "UTF-8");
+                break;
+            case 4:
+                userDicCon.convertNormal(this, " ", "\t", "UTF-8", "\r\n");
+                break;
+            default:
+                Toast.makeText(this, "すみません未実装です", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void selectConverterFromPobox() throws IOException, IndexOutOfBoundsException {
+        userDicCon.setInputPath("pobox/backup_dic/JPNUserDict.txt");
+        userDicCon.setOutputPath(outPosition);
+        switch (outPosition) {
+            case 0:
+                userDicCon.convertNormal(this, "\t", "\t", "UTF-8", "\t名\t\n");
+                break;
+            case 2:
+                userDicCon.convertToSimeji(this, "\t", "UTF-8");
+                break;
+            case 3:
+                userDicCon.convertNormal(this, "\t", " ", "UTF-8", "\n");
+                break;
+            default:
+                Toast.makeText(this, "すみません未実装です", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/akito/imedictconv/UserDictionaryConverter.java
@@ -0,0 +1,197 @@
+package com.example.akito.imedictconv;
+
+import android.app.AlertDialog;
+import android.os.Environment;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by akito on 2015/07/02.
+ */
+public class UserDictionaryConverter {
+    private String homePath;
+    private String inputPath;
+    private String outputPath;
+    private BufferedReader bufReader;
+    private PrintWriter printWriter;
+//    private String charSet;
+
+    public UserDictionaryConverter(String inpath, String outpath) {
+        homePath = Environment.getExternalStorageDirectory().getPath();
+        inputPath = homePath + "/" + inpath;
+        outputPath = homePath + "/" + outpath;
+        bufReader = null;
+        printWriter = null;
+    }
+    public UserDictionaryConverter() {
+        this("", "");
+    }
+
+    public String getOutputPath() {
+        return outputPath;
+    }
+
+    public void setInputPath(String inpath) {
+        inputPath = homePath + "/" + inpath;
+    }
+    public void setOutputPath(int outNumber) {
+        switch (outNumber) {
+            case 0:
+                outputPath = homePath + "/GoogleIMEDic.txt";
+                break;
+            case 1:
+                outputPath = homePath + "/ATOK/ATOK_user_dic.txt";
+                break;
+            case 2:
+                outputPath = homePath + "/Simeji/simeji_user_dic.txt";
+                break;
+            case 3:
+                outputPath = homePath + "/FlickWnn/user_dic.txt";
+                break;
+            case 4:
+                outputPath = homePath + "/pobox/backup_dic/JPNUserDict.txt";
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void closeFile() {
+        if (bufReader != null ) try {bufReader.close();} catch (IOException e) {}
+        if (printWriter != null ) printWriter.close();
+    }
+
+
+
+    public void convertNormal(MainActivity ma, String inSeparator, String outSeparator, String charSet, String lf_code) throws IOException, IndexOutOfBoundsException {
+        if (!checkBeforeRead(ma, charSet)) return;
+        checkBeforeWrite(charSet);
+
+        String str = null;
+        if (inputPath.equals(homePath + "/pobox/backup_dic/JPNUserDict.txt")) {
+            while ((str = bufReader.readLine()) != null) {
+                if(!str.startsWith(";")) {
+                    printWriter.print( str.replaceFirst("\t", outSeparator) + lf_code);
+                }
+            }
+
+        } else if(inputPath.equals(homePath + "/GoogleIMEDic.txt")) {
+            Pattern p = Pattern.compile("(^.+\t.+)(\t.+\t$)$");
+            while ((str = bufReader.readLine()) != null) {
+                Matcher m = p.matcher(str);
+                if (m.find()) {
+                    printWriter.print(m.group(1).replaceFirst("\t", outSeparator) + lf_code);
+                }
+            }
+
+        } else if(inSeparator.equals(outSeparator)) {
+            while ((str = bufReader.readLine()) != null) {
+                printWriter.print(str+lf_code);
+            }
+
+        } else {
+            while ((str = bufReader.readLine()) != null) {
+                printWriter.print(str.replaceFirst(inSeparator, outSeparator) + lf_code);
+            }
+        }
+
+        Toast.makeText(ma, outputPath + "に変換ファイルを保存しました", Toast.LENGTH_LONG).show();
+    }
+
+    public void convertFromSimeji(MainActivity ma, String separator, String charSet, String lf_code) throws IOException, IndexOutOfBoundsException {
+        if (!checkBeforeRead(ma, charSet)) return;
+        checkBeforeWrite(charSet);
+
+        String data[] = (bufReader.readLine()).split("\"\\],\"JAJP_KEY\":\\[\"");
+        String value[] = data[0].split("\",\"");
+        String key[] = data[1].split("\",\"");
+        int last = value.length - 1;
+
+        if (last > 0) {
+            value[0] = value[0].substring(42);
+            key[last] = key[last].substring(0, key[last].length() - 3);
+        } else {
+            // 辞書登録数が1のときの処理
+            return;
+        }
+
+        for (int i = 0; i <= last; i++) {
+            printWriter.print(key[i] + separator + value[i] + lf_code);
+        }
+        Toast.makeText(ma, outputPath + "に変換ファイルを保存しました", Toast.LENGTH_LONG).show();
+    }
+
+    public void convertToSimeji(MainActivity ma, String separator, String charSet) throws IOException, IndexOutOfBoundsException {
+        if (!checkBeforeRead(ma, charSet)) return;
+        checkBeforeWrite(charSet);
+
+        ArrayList<String> value = new ArrayList<>();
+        ArrayList<String> key = new ArrayList<>();
+        String str = null;
+        if (inputPath.equals(homePath + "/pobox/backup_dic/JPNUserDict.txt")) {
+//            Pattern p = Pattern.compile("^;.*");
+            while ((str = bufReader.readLine()) != null) {
+//                if (!p.matcher(str).matches()) {
+                if(!str.startsWith(";")) {
+                    String[] tmp = str.split(separator);
+                    value.add(tmp[0]);
+                    key.add(tmp[1]);
+                }
+            }
+        } else {
+            while ((str = bufReader.readLine()) != null) {
+                String[] tmp = str.split(separator);
+                value.add(tmp[0]);
+                key.add(tmp[1]);
+            }
+        }
+
+        int last = value.size() - 1;
+        //書き込み
+        printWriter.print("{\"EN_KEY\":[],\"EN_VALUE\":[],\"JAJP_VALUE\":[");
+        for (int i = 0; i < last; i++) {
+            printWriter.print("\"" + key.get(i) + "\",");
+        }
+        printWriter.print("\"" + key.get(last) + "\"],\"JAJP_KEY\":[");
+        for (int i = 0; i < last; i++) {
+            printWriter.print("\"" + value.get(i) + "\",");
+        }
+        printWriter.print("\"" + value.get(last) + "\"]}" + "\n");
+        Toast.makeText(ma, outputPath + "に変換ファイルを保存しました", Toast.LENGTH_LONG).show();
+    }
+
+
+    private boolean checkBeforeRead(MainActivity ma, String charSet) throws IOException {
+        File file = new File(inputPath);
+        if (file.exists()) if (file.isFile()) if (file.canRead()) {
+            bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(inputPath), charSet));
+            return true;
+        }
+        new AlertDialog.Builder(ma)
+                .setPositiveButton("OK", null)
+                .setMessage(inputPath + "が読み込めませんでした\n\n" + "移行元IMEのユーザー辞書をエキスポートしてから再度お試しください").show();
+        return false;
+    }
+
+    private void checkBeforeWrite(String charSet) throws IOException {
+        File fi = new File(outputPath);
+        File dir = fi.getParentFile();
+        if (dir != null && !dir.exists()) {
+            dir.mkdirs();
+        }
+        printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath), charSet)));
+    }
+
+}
